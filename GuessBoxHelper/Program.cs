66410921371f5bea2ed9df24cace1142690d1607
彤崽 <z406121605@gmail.com>
2025-06-27 11:52:05 +0800

    Render部署：添加PORT监听支持

--- GuessBoxHelper/Program.cs
+++ GuessBoxHelper/Program.cs
@@ -1,8 +1,12 @@
-using GuessBoxHelper.Data;
+﻿using GuessBoxHelper.Data;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// ✅ Render 要求监听环境变量指定的端口
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+builder.WebHost.UseUrls($"http://*:{port}");
 
 // Add services to the container.
 builder.Services.AddRazorPages();
@@ -15,12 +19,11 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    // The default HSTS value is 30 days. You may want to change this for production scenarios.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
-
 app.UseStaticFiles();
 
 app.UseRouting();
